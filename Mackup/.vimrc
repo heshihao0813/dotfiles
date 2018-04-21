3c87525d4eef9ca8fcb72d54187994aaa31658d7
Shihao He <heshihao.bn@gmail.com>
2018-04-21 19:51:22 +0800

    Weekly update

--- Mackup/.vimrc
+++ Mackup/.vimrc
@@ -1,13 +1,3 @@
 set relativenumber
 set number
-
-nnoremap yy "+yy
-vnoremap y "+y
-
-nnoremap p "+p
-vnoremap p "+p
-nnoremap P "+P
-vnoremap P "+P
-
-nnoremap dd "+dd
-vnoremap d "+d
+set clipboard=unnamed
---
+++ Mackup/.vscode/extensions/shd101wyy.markdown-preview-enhanced-0.3.5/node_modules/@shd101wyy/mume/out/src/markdown-engine-config.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.defaultMarkdownEngineConfig = {
+    usePandocParser: false,
+    breakOnSingleNewLine: true,
+    enableTypographer: false,
+    enableWikiLinkSyntax: true,
+    enableEmojiSyntax: true,
+    enableExtendedTableSyntax: false,
+    enableCriticMarkupSyntax: false,
+    wikiLinkFileExtension: ".md",
+    protocolsWhiteList: "http://, https://, atom://, file://, mailto:, tel:",
+    mathRenderingOption: "KaTeX",
+    mathInlineDelimiters: [["$", "$"], ["\\(", "\\)"]],
+    mathBlockDelimiters: [["$$", "$$"], ["\\[", "\\]"]],
+    codeBlockTheme: "auto.css",
+    previewTheme: "github-light.css",
+    revealjsTheme: "white.css",
+    mermaidTheme: "mermaid.css",
+    frontMatterRenderingOption: "none",
+    imageFolderPath: "/assets",
+    printBackground: false,
+    phantomPath: "phantomjs",
+    pandocPath: "pandoc",
+    pandocMarkdownFlavor: "markdown-raw_tex+tex_math_single_backslash",
+    pandocArguments: [],
+    latexEngine: "pdflatex",
+    enableScriptExecution: false,
+};
+//# sourceMappingURL=markdown-engine-config.js.map
